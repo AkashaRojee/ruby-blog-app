3c1a1ab942bdb19c123fc7239a6a9345daa3ecd6
Akasha <Akasha.Rojee@gmail.com>
2021-11-05 16:21:11 +0400

    Return 3 most recent posts for given user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
+
+  def three_most_recent_posts
+    Post.find(User).order(created_at: :desc).limit(3)
+  end
 end
