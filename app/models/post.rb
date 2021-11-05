38306ff37854f75eb4e764108c1316c05c5a5c6e
Akasha <Akasha.Rojee@gmail.com>
2021-11-05 16:44:18 +0400

    Return 5 most recent comments for given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,9 @@ class Post < ApplicationRecord
 
   def increment_counter
     user.update(posts_counter: user.posts_counter + 1)
+  end
+
+  def recent_comments(limit = 5)
+    comments.last(limit)
+  end
 end
