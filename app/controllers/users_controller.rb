a1d66cc9e870b7c1ed0b0d6eb7502c902624aacc
Akasha <Akasha.Rojee@gmail.com>
2021-11-05 21:12:10 +0400

    Add view for user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  def show(id)
-    @user = User.find(id)
+  def show
+    @user = User.find(params[:id])
     @posts = @user.recent_posts
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,20 +17,20 @@ ActiveRecord::Schema.define(version: 2021_10_29_055136) do
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
-    t.bigint "author_id", null: false
+    t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "author_id", null: false
+    t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
@@ -39,10 +39,10 @@ ActiveRecord::Schema.define(version: 2021_10_29_055136) do
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
-    t.bigint "author_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_posts_on_author_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,8 +55,8 @@ ActiveRecord::Schema.define(version: 2021_10_29_055136) do
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users", column: "author_id"
-  add_foreign_key "posts", "users", column: "author_id"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users"
 end
