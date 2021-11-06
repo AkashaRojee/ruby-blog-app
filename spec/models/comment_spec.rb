eb301ae3302afea37ff3bc249768ad009382e51f
Akasha <Akasha.Rojee@gmail.com>
2021-11-06 13:43:38 +0400

    Fix linter errors

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   describe '#increment_counter' do
-    user = User.create(name: "User 1", bio: "User bio")
-    post = Post.create(title: "Post 1", text: "Sample post text", user: user)
-    subject { Comment.create(text: "Comment", post: post, user: user) }
+    user = User.create(name: 'User 1', bio: 'User bio')
+    post = Post.create(title: 'Post 1', text: 'Sample post text', user: user)
+    subject { Comment.create(text: 'Comment', post: post, user: user) }
 
     it 'should increment the post comments counter' do
       subject.increment_counter
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,13 +24,12 @@ RSpec.describe User, type: :model do
 
   describe '#recent_posts' do
     it 'should return recent posts' do
-
-      user = User.create(name: "User 1", bio: "User bio")
-      post = Post.create(title: "Post 1", text: "Sample post text", user: user)
-      Comment.create(text: "Comment 1", post: post, user: user)
-      Comment.create(text: "Comment 2", post: post, user: user)
-      Comment.create(text: "Comment 3", post: post, user: user)
-      Comment.create(text: "Comment 4", post: post, user: user)
+      user = User.create(name: 'User 1', bio: 'User bio')
+      post = Post.create(title: 'Post 1', text: 'Sample post text', user: user)
+      Comment.create(text: 'Comment 1', post: post, user: user)
+      Comment.create(text: 'Comment 2', post: post, user: user)
+      Comment.create(text: 'Comment 3', post: post, user: user)
+      Comment.create(text: 'Comment 4', post: post, user: user)
 
       subject { user }
       expect(subject.recent_posts.length).to be <= 3
