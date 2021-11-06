c2154310ac1e7c83e722936c89b5f89d708d2d70
Akasha <Akasha.Rojee@gmail.com>
2021-11-06 09:43:11 +0400

    Save like on post and increment likes counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  after_save :increment_counter
+
   def increment_counter
     post.update(comments_counter: posts.comments_counter + 1)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  after_save :increment_counter
+
   def increment_counter
     post.update(likes_counter: post.likes_counter + 1)
   end
