cfe78cd0aa04b3f4b040ffbf4381aac365d97b49
Akasha <Akasha.Rojee@gmail.com>
2021-11-06 10:15:32 +0400

    Validate post fields

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, :likes_counter, numericality: { greater_than_or_equal_to: 0 }
+
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
