a39ae294be2b4675c93d4a6b31e1ee4d8693a89b
Akasha <Akasha.Rojee@gmail.com>
2021-11-06 10:18:12 +0400

    Add user unit spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    subject { User.new name: 'John Smith' }
+
+    before { subject.save }
+
+    it 'should have a name' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'should have a integer posts counter greater than or equal to zero' do
+      subject.posts_counter = -1
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe '#recent_posts' do
+    subject { User.first }
+
+    it 'should return 3 most recent posts' do
+      expect(subject.recent_posts.length).to be(3)
+    end
+  end
 end
